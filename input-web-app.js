7c6dc432318c01e4290156cb4a885e513402b0a5
darren <darrenarmani666@gmail.com>
2023-12-05 10:35:32 +0700

    darren

---
+++ input-web-app.js
@@ -0,0 +1,16 @@
+const scriptURL = '<put script here>'
+const form = document.forms['submit-to-google-sheet']
+const msg = document.getElementById("msg")
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+        .then(response => {
+            msg.innerHTML = "Message Sent Successfully"
+            setTimeout(function () {
+                msg.innerHTML = ""
+            }, 5000)
+            form.reset()
+        })
+        .catch(error => console.error('Error!', error.message))
+})
